443aa985dcee120e40663121a21e56d1190075a6
Roger <11964962+arcataroger@users.noreply.github.com>
2020-09-01 10:40:55 -0700

    Cleanup

--- src/index.js
+++ src/index.js
@@ -1,89 +1,82 @@
 import "./index.css";
 
-import React, {
-    useState,
-    useEffect
-} from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import JavascriptTimeAgo from 'javascript-time-ago';
-import en from 'javascript-time-ago/locale/en';
-import ReactTimeAgo from 'react-time-ago';
+import JavascriptTimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
+import ReactTimeAgo from "react-time-ago";
 import fileSize from "filesize";
-
 JavascriptTimeAgo.addLocale(en);
 
 function Resources() {
-    const [data, setData] = useState(null);
-    useEffect(() => {
-        fetch(`https://reactor.fieldmuseum.dev/apiv2/resources`)
-            .then(res => res.json())
-            .then(setData)
-            .catch(console.error);
-    }, []);
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(`https://reactor.fieldmuseum.dev/apiv2/resources`)
+      .then((res) => res.json())
+      .then(setData)
+      .catch(console.error);
+  }, []);
 
-    if (data) {
+  if (data) {
+    const legend = data.taxonomy_legend;
 
-        const legend = data.taxonomy_legend;
+    return (
+      <div>
+        <h1>Resources</h1>
+        {data.resources.map((resource) => (
+          <div key={resource.nid}>
+            <h2>{resource.title}</h2>
+            <ul>
+              <li>NID: {resource.nid}</li>
+              <li>
+                Last modified: <ReactTimeAgo date={resource.timestamp * 1000} />
+              </li>
+              <li>
+                Files:
+                <ul>
+                  {resource.files &&
+                    resource.files.map((file) => (
+                      <li key={file.fid}>
+                        <a href={file.url}>{file.language}</a> {file.filemime}{" "}
+                        {fileSize(file.filesize, { base: 10 })}
+                      </li>
+                    ))}
+                </ul>
+              </li>
 
-        return (
-            <div>
-                <h1>Resources</h1>
-                {
-                    data.resources.map((resource) =>
-                        <div key={resource.nid}>
-                            <h2>{resource.title}</h2>
-                            <ul>
-                                <li>NID: {resource.nid}</li>
-                                <li>Last modified: <ReactTimeAgo date={resource.timestamp * 1000}/></li>
-                                <li>Files:
-                                    <ul>
-                                        {resource.files && resource.files.map((file) =>
-                                            <li key={file.fid}>
-                                                <a href={file.url}>{file.language}</a> {file.filemime} {fileSize(file.filesize, {base: 10})}
-                                            </li>
-                                        )}
-                                    </ul>
-                                </li>
-
-                                <li>Taxonomies:
-
-                                    {resource.taxonomies && resource.taxonomies.map((taxon) => {
-                                        return (
-                                            taxon.vid > 0 &&
-                                            <ul key={taxon.vid}>
-                                                <li>{legend[taxon.vid].machine_name} </li>
-                                                <ul>
-                                                    {taxon.terms.map((tid) => {
-                                                        const termName = legend[taxon.vid].terms[tid];
-                                                        return (
-                                                            <li key={tid}>
-                                                                {termName && termName.name}
-                                                            </li>
-                                                        )
-                                                    })}
-                                                </ul>
-                                            </ul>
-                                        )
-                                    })
-                                    }
-
-                                </li>
-                            </ul>
-                        </div>
-                    )}
-            </div>
-        );
-    } else {
-        return null;
-    }
+              <li>
+                Taxonomies:
+                {resource.taxonomies &&
+                  resource.taxonomies.map((taxon) => {
+                    return (
+                      taxon.vid > 0 && (
+                        <ul key={taxon.vid}>
+                          <li>{legend[taxon.vid].machine_name} </li>
+                          <ul>
+                            {taxon.terms.map((tid) => {
+                              const termData = legend[taxon.vid].terms[tid];
+                              return (
+                                <li key={tid}>{termData && termData.name}</li>
+                              );
+                            })}
+                          </ul>
+                        </ul>
+                      )
+                    );
+                  })}
+              </li>
+            </ul>
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
-
 
 function App() {
-    return <Resources/>;
+  return <Resources />;
 }
 
-ReactDOM.render(
-    <App/>,
-    document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
